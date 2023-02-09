7bff1e71c40b6a9d2b3ce6a942e90fc4770fafe5
Mint Missy <dawidkostka480@gmail.com>
2023-02-09 10:28:15 +0100

    refactor: remove shared module (dead code)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PyramidsModule } from './pyramids/pyramids.module';
 import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
+import { PyramidsModule } from './pyramids/pyramids.module';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule, AppRoutingModule, PyramidsModule, CoreModule, SharedModule],
+	imports: [BrowserModule, AppRoutingModule, PyramidsModule, CoreModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
